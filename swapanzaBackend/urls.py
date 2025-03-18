15831f2aa93ac4e8223f07e9f0da259c2ef0fa1b
Nikolay Nikolov <nikoloff.ftw@gmail.com>
2025-03-18 15:35:32 +0200

    fixed page refresh issues and made the backend stable

--- swapanzaBackend/urls.py
+++ swapanzaBackend/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chat.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', never_cache(TemplateView.as_view(template_name='index.html'))),
 ]
 
 if settings.DEBUG:
